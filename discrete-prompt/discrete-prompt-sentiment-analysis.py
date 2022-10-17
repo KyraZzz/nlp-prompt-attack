e05917db3733bceed06eb29d5845f60c8b65c94f
kyra-at-beyla <yz709@cam.ac.uk>
2022-10-17 16:53:25 +0100

    roberta-base model for sentiment analysis

--- discrete-prompt/discrete-prompt-sentiment-analysis.py
+++ discrete-prompt/discrete-prompt-sentiment-analysis.py
@@ -1,128 +1,113 @@
+import pandas as pd
+import numpy as np
+from tqdm.auto import tqdm
 import torch
 import torch.nn as nn
-from datasets import load_dataset
-from transformers import AutoModel, AutoTokenizer, AutoConfig, AdamW, get_linear_schedule_with_warmup
 from torch.utils.data import Dataset, DataLoader
+from transformers import AdamW, get_linear_schedule_with_warmup, AutoTokenizer, AutoModel, AutoConfig
 import pytorch_lightning as pl
-from pytorch_lightning.loggers import TensorBoardLogger
+from torchmetrics.functional import accuracy, auroc
 from pytorch_lightning.callbacks import ModelCheckpoint, EarlyStopping
+from pytorch_lightning.loggers import TensorBoardLogger
+from datasets import load_dataset
+import ipdb
+
+from sklearn.metrics import classification_report, multilabel_confusion_matrix
 
 PARAMS = {
-    # "data_path": "./datasets/qnli",
-    "data_path": "SetFit/qnli",
-    "model_name": "roberta-base",
     "batch_size": 12,
     "lr": 1e-3,
     "max_epochs": 1,
+    "num_label_columns": 1,
     "model_name": "roberta-base",
-    "max_token_count": 2048,
+    "max_token_count": 512,
     "random_seed": 42
 }
 pl.seed_everything(PARAMS['random_seed'])
 
-def preprocess_data():
-    raw_data = load_dataset(PARAMS["data_path"])
-    data_train = raw_data['train']
-    data_test = raw_data['test']
-    data_val = raw_data['validation']
-    return data_train, data_val, data_test
+def data_preprocess():
+    sst2 = load_dataset("sst2")
+    sst2_train = sst2['train']
+    sst2_val = sst2['validation']
+    sst2_test = sst2['test']
+    return sst2_train, sst2_val, sst2_test
 
-class QNLIDataset(Dataset):
-    def __init__(self, data, tokenizer, max_token_len, template):
+class SentimentDatset(Dataset):
+    def __init__(self, data, tokenizer):
         self.tokenizer = tokenizer
         self.data = data
-        self.max_token_len = max_token_len
-        self.template = template
     
     def __len__(self):
         return len(self.data)
-
-    def __getitem__(self, index):
-        row = self.data[index]
-        question = row['text1']
-        answer = row['text2']
-        label = row['label']
-        label_text = row['label_text']
-
-        # prompt = <question><SEP><template><answer><SEP> 
-        prompt = question + " " + self.template + ", " + answer
-        end_prompt = self.tokenizer.encode_plus(
-            prompt,
+    
+    def __getitem__(self, index: int):
+        data_row = self.data[index]
+        sentence = data_row['sentence']
+        labels = data_row['label']
+        encoding = self.tokenizer.encode_plus(
+            sentence,
             add_special_tokens=True,
-            max_length=self.max_token_len,
+            max_length=PARAMS["max_token_count"],
             return_token_type_ids=False,
             padding="max_length",
             truncation=True,
             return_attention_mask=True,
-            return_tensors="pt"
+            return_tensors='pt',
         )
-        
+
         return dict(
-            prompt=prompt,
-            input_ids=end_prompt["input_ids"].flatten(),
-            attention_mask=end_prompt["attention_mask"].flatten(),
-            label=torch.FloatTensor(label),
-            label_text=label_text
+            sentence=sentence,
+            input_ids=encoding["input_ids"].flatten(),
+            attention_mask=encoding["attention_mask"].flatten(),
+            labels=torch.FloatTensor([labels])
         )
     
-class QNLIDataModule(pl.LightningDataModule):
-    def __init__(self, train_data, val_data, test_data, tokenizer, template, batch_size, max_token_len):
+class SentimentDataModule(pl.LightningDataModule):
+    def __init__(self, train_data, val_data, test_data, tokenizer):
         super().__init__()
         self.train_data = train_data
         self.val_data = val_data
         self.test_data = test_data
         self.tokenizer = tokenizer
-        self.template = template
-        self.batch_size = batch_size
-        self.max_token_len = max_token_len
-        
+    
     def setup(self, stage=None):
-        self.train_dataset = QNLIDataset(
+        self.train_dataset = SentimentDatset(
             self.train_data,
-            self.tokenizer,
-            self.max_token_len,
-            self.template
+            self.tokenizer
         )
-        
-        self.val_dataset = QNLIDataset(
+
+        self.val_dataset = SentimentDatset(
             self.val_data,
-            self.tokenizer,
-            self.max_token_len,
-            self.template
+            self.tokenizer
         )
-        
-        self.test_dataset = QNLIDataset(
+    
+        self.test_dataset = SentimentDatset(
             self.test_data,
-            self.tokenizer,
-            self.max_token_len,
-            self.template
+            self.tokenizer
         )
-        
+    
     def train_dataloader(self):
         return DataLoader(
             self.train_dataset,
-            batch_size = self.batch_size,
-            shuffle = True,
-            num_workers = 1
+            batch_size=PARAMS["batch_size"],
+            shuffle=True
         )
     
     def val_dataloader(self):
         return DataLoader(
             self.val_dataset,
-            batch_size = self.batch_size,
-            shuffle = True,
-            num_workers = 1
+            batch_size=PARAMS["batch_size"],
+            shuffle=True
         )
     
     def test_dataloader(self):
         return DataLoader(
             self.test_dataset,
-            batch_size = self.batch_size,
-            shuffle = True,
-            num_workers = 1
+            batch_size=PARAMS["batch_size"],
+            shuffle=True
         )
 
-class QNLIClassifier(pl.LightningModule):
+class SentimentClassifier(pl.LightningModule):
     def __init__(self, n_classes: int, n_training_steps=None, n_warmup_steps=None):
         super().__init__()
         self.model = AutoModel.from_pretrained(PARAMS['model_name'], return_dict=True)
@@ -132,6 +117,11 @@ class QNLIClassifier(pl.LightningModule):
         self.criterion = nn.BCELoss()
     
     def forward(self, input_ids, attention_mask, labels=None):
+        # ipdb.set_trace()
+        """
+        output.last_hidden_state (batch_size, token_num, hidden_size): hidden representation for each token in each sequence of the batch. 
+        output.pooler_output (batch_size, hidden_size): take hidden representation of [CLS] token in each sequence, run through BertPooler module (linear layer with Tanh activation)
+        """
         output = self.model(input_ids, attention_mask=attention_mask)
         output = self.classifier(output.pooler_output)
         output = torch.sigmoid(output)
@@ -141,6 +131,7 @@ class QNLIClassifier(pl.LightningModule):
         return loss, output
 
     def training_step(self, batch, batch_idx):
+        # ipdb.set_trace()
         input_ids = batch["input_ids"]
         attention_mask = batch["attention_mask"]
         labels = batch["labels"]
@@ -149,6 +140,7 @@ class QNLIClassifier(pl.LightningModule):
         return {"loss": loss, "predictions": outputs, "labels": labels}
     
     def validation_step(self, batch, batch_idx):
+        # ipdb.set_trace()
         input_ids = batch["input_ids"]
         attention_mask = batch["attention_mask"]
         labels = batch["labels"]
@@ -157,6 +149,7 @@ class QNLIClassifier(pl.LightningModule):
         return loss
 
     def test_step(self, batch, batch_idx):
+        # ipdb.set_trace()
         input_ids = batch["input_ids"]
         attention_mask = batch["attention_mask"]
         labels = batch["labels"]
@@ -165,10 +158,11 @@ class QNLIClassifier(pl.LightningModule):
         return loss
     
     def configure_optimizers(self):
+        # ipdb.set_trace()
         optimizer = AdamW(self.parameters(), lr=PARAMS['lr'])
         scheduler = get_linear_schedule_with_warmup(
             optimizer,
-            num_warmup_steps=self.n_warmup_steps,
+            num_warmup_steps=self.n_warmup_steps, # very low learning rate
             num_training_steps=self.n_training_steps
         ) # learning rate scheduler
         
@@ -180,16 +174,16 @@ class QNLIClassifier(pl.LightningModule):
             )
         )
 
-def prepare_and_train(train_df, val_df, test_df, template):
+def prepare_and_train():
     """ Training best practices
         - Checkpointing that saves the best model based on validation loss
         - Logging the progress in TensorBoard
         - Early stopping that terminates the training when the loss has not improved for the last 2 epochs
     """
-    logger = TensorBoardLogger('/local/scratch-3/yz709/nlp-prompt-attack/tb_logs', name='discrete-prompt-roberta-base')
+    logger = TensorBoardLogger('/local/scratch-3/yz709/nlp-prompt-attack/tb_logs', name='discrete-prompt-sa-bert-noprompt')
     checkpoint_callback = ModelCheckpoint(
         dirpath='checkpoints',
-        filename='discrete-prompt-roberta-base-{epoch:02d}-{val_loss:.2f}',
+        filename='discrete-prompt-sa-bert-noprompt-{epoch:02d}-{val_loss:.2f}',
         verbose=True,
         monitor='val_loss',
         mode='min'
@@ -198,39 +192,37 @@ def prepare_and_train(train_df, val_df, test_df, template):
     
     # preprocess data
     tokenizer = AutoTokenizer.from_pretrained(PARAMS['model_name'])
-    data_module = QNLIDataModule(
-        train_df,
-        val_df,
-        test_df,
-        tokenizer,
-        template,
-        batch_size=PARAMS['batch_size'],
-        max_token_len=PARAMS['max_token_count']
+    train_data, val_data, test_data = data_preprocess()
+    data_module = SentimentDataModule(
+        train_data,
+        val_data,
+        test_data,
+        tokenizer
     )
 
     # model
-    steps_per_epoch = len(train_df) // PARAMS['batch_size']
+    steps_per_epoch = len(train_data) // PARAMS['batch_size']
     total_training_steps = steps_per_epoch * PARAMS['max_epochs']
     warmup_steps = total_training_steps // 5
-    model = QNLIClassifier(
-        n_classes=2,
+    model = SentimentClassifier(
+        n_classes=PARAMS['num_label_columns'],
         n_warmup_steps=warmup_steps,
         n_training_steps=total_training_steps
     )
     
     # train
     trainer = pl.Trainer(
-        fast_dev_run=True, # development mode
-        num_sanity_val_steps=2, # sanity check
+        # debugging purpose
+        fast_dev_run=7, # runs n batch of training, validation, test and prediction data through your trainer to see if there are any bugs
+        # ----------------
         logger = logger,
         callbacks=[early_stopping_callback,checkpoint_callback],
         max_epochs=PARAMS['max_epochs'],
         accelerator="gpu", 
-        devices=1,
-        # strategy="ddp"
+        gpus=[1],
+        # strategy="ddp",
     )
     trainer.fit(model, data_module)
 
 if __name__ == "__main__":
-    qnli_train, qnli_val, qnli_test = preprocess_data()
-    prepare_and_train(qnli_train, qnli_val, qnli_test, template = " <mask> ")
+    prepare_and_train()

0946b43e1c73ab626e3f0521eed6319cacdc13dd
kyra-jade2 <yz709@cam.ac.uk>
2022-10-21 09:41:36 +0100

    rmb to add with_prompt when testing prompt-based

--- experiments/discrete_prompt/run-te-jade-test.sh
+++ experiments/discrete_prompt/run-te-jade-test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --nodes=1
 #SBATCH --time=1:00:00
-#SBATCH --job-name=no-prompt
+#SBATCH --job-name=manual1
 #SBATCH --gres=gpu:1
 
 # run the application
@@ -12,9 +12,12 @@ conda activate nlp-prompt-attack-env                          # activate target 
 
 cd /jmain02/home/J2AD015/axf03/yxz79-axf03/nlp-prompt-attack/discrete-prompt
 python3 run.py \
-    --task_name "test-qnli-roberta-base-no-prompt" \
+    --task_name "test-qnli-roberta-base-manual-prompt-1" \
     --model_name_or_path "roberta-base" \
     --data_path "/jmain02/home/J2AD015/axf03/yxz79-axf03/nlp-prompt-attack/discrete-prompt/datasets/qnli" \
     --do_test \
-    --ckpt_path "/jmain02/home/J2AD015/axf03/yxz79-axf03/nlp-prompt-attack/discrete-prompt/checkpoints/qnli-roberta-base-no-prompt-epoch=10-val_loss=0.21.ckpt" \
+    --with_prompt \
+    --template "<cls> <question> ? <mask> , <answer> ." \
+    --verbalizer_dict '{"0":["Yes"], "1":["No"]}' \
+    --ckpt_path "/jmain02/home/J2AD015/axf03/yxz79-axf03/nlp-prompt-attack/discrete-prompt/checkpoints/qnli-roberta-base-manual-prompt-manual-1-epoch=08-val_loss=0.22.ckpt" \
     --num_gpu_devices 1

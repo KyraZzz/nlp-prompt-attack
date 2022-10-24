65ca65746a5b7b5cc33b2afc4a92275b15529528
kyra-at-beyla <yz709@cam.ac.uk>
2022-10-24 18:32:22 +0100

    change job submission script in CL platform

--- experiments/discrete_prompt/run-te.sh
+++ experiments/discrete_prompt/run-te.sh
@@ -11,13 +11,13 @@ python3 run.py \
     --task_name "qnli-roberta-base-manual-prompt-1" \
     --model_name_or_path "roberta-base" \
     --dataset_name "QNLI" \
-    --data_path "/local/scratch-3/yz709/nlp-prompt-attack/discrete-prompt/datasets/k_shot/k=1000/seed=13/QNLI" \
+    --data_path "/local/scratch-3/yz709/nlp-prompt-attack/discrete-prompt/datasets/k_shot/k=16/seed=13/QNLI" \
     --do_k_shot \
-    --k_samples_per_class 1000 \
+    --k_samples_per_class 16 \
     --do_train \
     --with_prompt \
-    --template "<cls> <question> ? <mask> , <answer> ." \
-    --verbalizer_dict '{"0":["ĠYes"], "1":["ĠNo"]}' \
+    --template "<cls> <question> ? <mask> , <sentence> ." \
+    --verbalizer_dict '{"0":["Yes"], "1":["No"]}' \
     --log_every_n_steps 100 \
     --batch_size 10 \
     --learning_rate 1e-5 \

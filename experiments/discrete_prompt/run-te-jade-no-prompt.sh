1f8b08dea56b94d45ff48bf0013e82ac5ab00874
kyra-jade2 <yz709@cam.ac.uk>
2022-10-25 23:56:06 +0100

    delete useless shell script

--- experiments/discrete_prompt/run-te-jade-no-prompt.sh
+++ experiments/discrete_prompt/run-te-jade-no-prompt.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --nodes=1
 #SBATCH --time=1:00:00
-#SBATCH --job-name=no-prompt
+#SBATCH --job-name=no-100
 #SBATCH --gres=gpu:1
 
 # run the application
@@ -10,17 +10,17 @@ module purge                                                  # Removes all modu
 source /jmain02/apps/python3/anaconda3/etc/profile.d/conda.sh # enable conda
 conda activate nlp-prompt-attack-env                          # activate target env
 
+seed_all=100
 cd /jmain02/home/J2AD015/axf03/yxz79-axf03/nlp-prompt-attack/discrete-prompt
 python3 run.py \
-    --random_seed 13 \
-    --task_name "qnli-roberta-large-manual-no-prompt-k16-seed13" \
+    --random_seed ${seed_all} \
+    --task_name "qnli-roberta-large-manual-no-prompt-k16-seed"${seed_all} \
     --model_name_or_path "roberta-large" \
     --dataset_name "QNLI" \
-    --data_path "/jmain02/home/J2AD015/axf03/yxz79-axf03/nlp-prompt-attack/discrete-prompt/datasets/k_shot/k=16/seed=13/QNLI" \
+    --data_path "/jmain02/home/J2AD015/axf03/yxz79-axf03/nlp-prompt-attack/discrete-prompt/datasets/k_shot/k=16/seed="${seed_all}"/QNLI" \
     --do_k_shot \
     --k_samples_per_class 16 \
     --do_train \
-    --do_test \
     --log_every_n_steps 20 \
     --batch_size 4 \
     --learning_rate 2e-5 \

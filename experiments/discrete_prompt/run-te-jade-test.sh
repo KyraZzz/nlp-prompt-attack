83317385cf061b76b7dc1daf1451e924255c36df
kyra-jade2 <yz709@cam.ac.uk>
2022-10-26 00:41:45 +0100

    test script format

--- experiments/discrete_prompt/run-te-jade-test.sh
+++ experiments/discrete_prompt/run-te-jade-test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --nodes=1
 #SBATCH --time=10:00:00
-#SBATCH --job-name=t-no-100
+#SBATCH --job-name=t-m6-87
 #SBATCH --gres=gpu:4
 
 # run the application
@@ -10,10 +10,10 @@ module purge                                                  # Removes all modu
 source /jmain02/apps/python3/anaconda3/etc/profile.d/conda.sh # enable conda
 conda activate nlp-prompt-attack-env                          # activate target env
 
-seed_all=100
+seed_all=87
 k_all=16
-ckpt_path_all="/jmain02/home/J2AD015/axf03/yxz79-axf03/nlp-prompt-attack/discrete-prompt/checkpoints/10-26/qnli-roberta-large-manual-no-prompt-k16-seed100/qnli-roberta-large-manual-no-prompt-k16-seed100-date=10-26H0M2-epoch=24-val_loss=0.60.ckpt"
-prompt_num=0
+ckpt_path_all="/jmain02/home/J2AD015/axf03/yxz79-axf03/nlp-prompt-attack/discrete-prompt/checkpoints/10-25/qnli-roberta-large-manual-prompt-6-k16-seed87/qnli-roberta-large-manual-prompt-6-k16-seed87-date=10-25H20M21-epoch=00-val_loss=0.73.ckpt"
+prompt_num=6
 # don't forget to change the template !!!
 cd /jmain02/home/J2AD015/axf03/yxz79-axf03/nlp-prompt-attack/discrete-prompt
 python3 run.py \
@@ -26,6 +26,9 @@ python3 run.py \
     --k_samples_per_class ${k_all} \
     --do_test \
     --ckpt_path ${ckpt_path_all} \
+    --with_prompt \
+    --template "<cls> <sentence> ? <mask> , <sentence>" \
+    --verbalizer_dict '{"0":["Yes"], "1":["No"]}' \
     --log_every_n_steps 20 \
     --batch_size 4 \
     --learning_rate 2e-5 \

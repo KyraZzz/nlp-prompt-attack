872f00fec1be55b78d7fe50e7081fa40ea1e632c
kyra-jade2 <yz709@cam.ac.uk>
2022-10-26 13:52:42 +0100

    separate matched and mismatched data for MNLI

--- discrete-prompt/prep_data.py
+++ discrete-prompt/prep_data.py
@@ -86,7 +86,89 @@ class MNLIPrepData(PrepData):
         res = val_test_dataset.train_test_split(test_size=0.5)
         self.val, self.test = res['train'], res['test']
         return self.train, self.val, self.test
-        
+
+class MNLIMatchedPrepData(PrepData):
+    """
+    MNLI dataset
+    DatasetDict({
+    train: Dataset({
+        features: ['premise', 'hypothesis', 'label', 'idx'],
+        num_rows: 392702
+        })
+    validation_matched: Dataset({
+        features: ['premise', 'hypothesis', 'label', 'idx'],
+        num_rows: 9815
+        })
+    validation_mismatched: Dataset({
+        features: ['premise', 'hypothesis', 'label', 'idx'],
+        num_rows: 9832
+        })
+    test_matched: Dataset({
+        features: ['premise', 'hypothesis', 'label', 'idx'],
+        num_rows: 9796
+        })
+    test_mismatched: Dataset({
+        features: ['premise', 'hypothesis', 'label', 'idx'],
+        num_rows: 9847
+        })
+    })
+    """
+    def __init__(self, data_path, random_seed, k = None):
+        super().__init__(data_path, random_seed, k)
+        if self.raw_dataset is None and k is None:
+            self.raw_dataset = load_dataset("glue", "mnli")
+    
+    def preprocess(self):
+        if self.train is not None and self.val is not None and self.test is not None:
+            return self.train, self.val, self.test
+        dataset = concatenate_datasets([self.raw_dataset["train"], self.raw_dataset["validation_matched"]]).shuffle(seed=self.random_seed)
+        res = dataset.train_test_split(test_size=0.2)
+        self.train, val_test_dataset = res['train'], res['test']
+        res = val_test_dataset.train_test_split(test_size=0.5)
+        self.val, self.test = res['train'], res['test']
+        return self.train, self.val, self.test
+
+class MNLIMisMatchedPrepData(PrepData):
+    """
+    MNLI dataset
+    DatasetDict({
+    train: Dataset({
+        features: ['premise', 'hypothesis', 'label', 'idx'],
+        num_rows: 392702
+        })
+    validation_matched: Dataset({
+        features: ['premise', 'hypothesis', 'label', 'idx'],
+        num_rows: 9815
+        })
+    validation_mismatched: Dataset({
+        features: ['premise', 'hypothesis', 'label', 'idx'],
+        num_rows: 9832
+        })
+    test_matched: Dataset({
+        features: ['premise', 'hypothesis', 'label', 'idx'],
+        num_rows: 9796
+        })
+    test_mismatched: Dataset({
+        features: ['premise', 'hypothesis', 'label', 'idx'],
+        num_rows: 9847
+        })
+    })
+    """
+    def __init__(self, data_path, random_seed, k = None):
+        super().__init__(data_path, random_seed, k)
+        if self.raw_dataset is None and k is None:
+            self.raw_dataset = load_dataset("glue", "mnli")
+    
+    def preprocess(self):
+        if self.train is not None and self.val is not None and self.test is not None:
+            return self.train, self.val, self.test
+        dataset = concatenate_datasets([self.raw_dataset["train"], self.raw_dataset['validation_mismatched']]).shuffle(seed=self.random_seed)
+        res = dataset.train_test_split(test_size=0.2)
+        self.train, val_test_dataset = res['train'], res['test']
+        res = val_test_dataset.train_test_split(test_size=0.5)
+        self.val, self.test = res['train'], res['test']
+        return self.train, self.val, self.test
+
 class SST2PrepData(QNLIPrepData):
     """
     SST-2 dataset
@@ -124,6 +206,10 @@ def data_preprocess(dataset_name, data_path, random_seed, k, do_k_shot=False):
             data_obj = QNLIPrepData(data_path, random_seed, k)
         case "MNLI":
             data_obj = MNLIPrepData(data_path, random_seed, k)
+        case "MNLI-MATCHED":
+            data_obj = MNLIMatchedPrepData(data_path, random_seed, k)
+        case "MNLI-MISMATCHED":
+            data_obj = MNLIMisMatchedPrepData(data_path, random_seed, k)
         case "SST2":
             data_obj = SST2PrepData(data_path, random_seed, k)
         case _:

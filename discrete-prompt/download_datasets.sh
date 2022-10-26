cf15b5a725b7724c0053b8fd0e8b21b9c400f04c
kyra-jade2 <yz709@cam.ac.uk>
2022-10-26 17:28:54 +0100

    save tb logs for MNLI prompt 5/6 experiment

--- discrete-prompt/download_datasets.sh
+++ discrete-prompt/download_datasets.sh
@@ -2,8 +2,8 @@
 tmpfile=$(mktemp ./temp.XXXXXX)
 mkdir -p ./datasets
 python3 prep_data.py \
-    --dataset_name "MNLI-MATCHED" \
-    --data_save_path "./datasets/mnli-matched" \
+    --dataset_name "MNLI-MISMATCHED" \
+    --data_save_path "./datasets/mnli-mismatched" \
     1> ${tmpfile} 2>&1
 
 rm ${tmpfile}

479ea571e0b50969800e7138b805d659b80bb91d
kyra-jade2 <yz709@cam.ac.uk>
2022-10-28 16:58:58 +0100

    run k = 100 and 1000 cases

--- experiments/discrete_prompt/run-te-jade-no-prompt.sh
+++ experiments/discrete_prompt/run-te-jade-no-prompt.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH --nodes=1
 #SBATCH --time=10:00:00
-#SBATCH --job-name=no1k-100
-#SBATCH --gres=gpu:4
+#SBATCH --job-name=qno1k-100
+#SBATCH --gres=gpu:8
 
 # run the application
 . /etc/profile.d/modules.sh                                   # Leave this line (enables the module command)
@@ -15,18 +15,18 @@ k_all=1000
 cd /jmain02/home/J2AD015/axf03/yxz79-axf03/nlp-prompt-attack/discrete-prompt
 python3 run.py \
     --random_seed ${seed_all} \
-    --task_name "mnli-mismatched-roberta-large-manual-no-prompt-k"${k_all}"-seed"${seed_all} \
+    --task_name "qnli-roberta-large-manual-no-prompt-k"${k_all}"-seed"${seed_all} \
     --model_name_or_path "roberta-large" \
-    --dataset_name "MNLI-MISMATCHED" \
-    --data_path "/jmain02/home/J2AD015/axf03/yxz79-axf03/nlp-prompt-attack/discrete-prompt/datasets/k_shot/k="${k_all}"/seed="${seed_all}"/MNLI-MISMATCHED" \
-    --n_classes 3 \
+    --dataset_name "QNLI" \
+    --data_path "/jmain02/home/J2AD015/axf03/yxz79-axf03/nlp-prompt-attack/discrete-prompt/datasets/k_shot/k="${k_all}"/seed="${seed_all}"/QNLI" \
+    --n_classes 2 \
     --do_k_shot \
-    --k_samples_per_class 16 \
+    --k_samples_per_class ${k_all} \
     --do_train \
     --do_test \
     --log_every_n_steps 20 \
     --batch_size 4 \
     --learning_rate 2e-5 \
-    --num_gpu_devices 1 \
+    --num_gpu_devices 8 \
     --max_epoch 250 \
-    --early_stopping_patience 20
+    --early_stopping_patience 10
--- experiments/discrete_prompt/run-te-jade-with-prompt.sh
+++ experiments/discrete_prompt/run-te-jade-with-prompt.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH --nodes=1
 #SBATCH --time=10:00:00
-#SBATCH --job-name=mm1k-100
-#SBATCH --gres=gpu:4
+#SBATCH --job-name=q41k-100
+#SBATCH --gres=gpu:8
 
 # run the application
 . /etc/profile.d/modules.sh                                   # Leave this line (enables the module command)
@@ -12,25 +12,25 @@ conda activate nlp-prompt-attack-env                          # activate target 
 
 seed_all=100
 k_all=1000
-prompt_num=1
+prompt_num=4
 cd /jmain02/home/J2AD015/axf03/yxz79-axf03/nlp-prompt-attack/discrete-prompt
 python3 run.py \
     --random_seed ${seed_all} \
-    --task_name "mnli-mismatched-roberta-large-manual-prompt-"${prompt_num}"-k"${k_all}"-seed"${seed_all} \
+    --task_name "qnli-roberta-large-manual-prompt-"${prompt_num}"-k"${k_all}"-seed"${seed_all} \
     --model_name_or_path "roberta-large" \
-    --dataset_name "MNLI-MISMATCHED" \
-    --data_path "/jmain02/home/J2AD015/axf03/yxz79-axf03/nlp-prompt-attack/discrete-prompt/datasets/k_shot/k="${k_all}"/seed="${seed_all}"/MNLI-MISMATCHED" \
-    --n_classes 3 \
+    --dataset_name "QNLI" \
+    --data_path "/jmain02/home/J2AD015/axf03/yxz79-axf03/nlp-prompt-attack/discrete-prompt/datasets/k_shot/k="${k_all}"/seed="${seed_all}"/QNLI" \
+    --n_classes 2 \
     --do_k_shot \
     --k_samples_per_class ${k_all} \
     --do_train \
     --do_test \
     --with_prompt \
-    --template "<cls> <premise> ? <mask> , <hypothesis> ." \
-    --verbalizer_dict '{"0":["Yes"], "1":["Maybe"], "2":["No"]}' \
+    --template "<cls> <sentence> ? <mask> , <question> ." \
+    --verbalizer_dict '{"0":["Yes"], "1":["No"]}' \
     --log_every_n_steps 20 \
     --batch_size 4 \
     --learning_rate 2e-5 \
-    --num_gpu_devices 4 \
+    --num_gpu_devices 8 \
     --max_epoch 250 \
-    --early_stopping_patience 20
+    --early_stopping_patience 10

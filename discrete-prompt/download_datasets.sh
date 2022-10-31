5f45dc8ae358f2f70c67af8d212d92c883204f0a
kyra-jade2 <yz709@cam.ac.uk>
2022-10-31 17:23:03 +0000

    clean up repository

--- discrete-prompt/download_datasets.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-tmpfile=$(mktemp ./temp.XXXXXX)
-mkdir -p ./datasets
-python3 prep_data.py \
-    --dataset_name "MNLI-MISMATCHED" \
-    --data_save_path "./datasets/mnli-mismatched" \
-    1> ${tmpfile} 2>&1
-
-rm ${tmpfile}

119fc06865a2ff19c7fb896ea88055ad9cc92728
kyra-jade2 <yz709@cam.ac.uk>
2022-10-31 17:33:10 +0000

    update scripts to be more general

--- utils/download_datasets.sh
+++ utils/download_datasets.sh
@@ -3,7 +3,7 @@ tmpfile=$(mktemp ./temp.XXXXXX)
 mkdir -p ./datasets
 python3 prep_data.py \
     --dataset_name "MNLI-MISMATCHED" \
-    --data_save_path "./datasets/mnli-mismatched" \
+    --data_save_path "~/nlp-prompt-attack/datasets/mnli-mismatched" \
     1> ${tmpfile} 2>&1
 
 rm ${tmpfile}

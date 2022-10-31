4da6f314aa5ea97e6e4387ffb61d65389d9f1f39
kyra-jade2 <yz709@cam.ac.uk>
2022-10-31 17:22:05 +0000

    clean up repository

---
+++ src/models.py
@@ -0,0 +1,217 @@
+import torch
+import torch.nn as nn
+from torch.optim import AdamW
+from transformers import get_linear_schedule_with_warmup, AutoModel, AutoModelForMaskedLM
+import pytorch_lightning as pl
+from torchmetrics import Accuracy
+
+class TextEntailClassifier(pl.LightningModule):
+    def __init__(self, model_name, n_classes, learning_rate, n_training_steps=None, n_warmup_steps=None):
+        super().__init__()
+        self.model = AutoModel.from_pretrained(model_name, return_dict=True)
+        self.classifier = nn.Linear(self.model.config.hidden_size, n_classes)
+        self.n_classes = n_classes
+        self.learning_rate = learning_rate
+        self.n_training_steps = n_training_steps
+        self.n_warmup_steps = n_warmup_steps
+        self.criterion = nn.CrossEntropyLoss() # loss function for classification problem
+        self.accuracy = Accuracy(dist_sync_on_step=True)
+        self.train_loss_arr = []
+        self.train_acc_arr = []
+        self.val_loss_arr = []
+        self.val_acc_arr = []
+        self.test_loss_arr = []
+        self.test_acc_arr = []
+        self.save_hyperparameters()
+    
+    def forward(self, input_ids, attention_mask, labels=None):
+        """
+        output.last_hidden_state (batch_size, token_num, hidden_size): hidden representation for each token in each sequence of the batch. 
+        output.pooler_output (batch_size, hidden_size): take hidden representation of [CLS] token in each sequence, run through BertPooler module (linear layer with Tanh activation)
+        """
+        output = self.model(input_ids, attention_mask=attention_mask)
+        output = self.classifier(output.pooler_output)
+        output = torch.sigmoid(output)
+        loss = 0
+        if labels is not None:
+            loss = self.criterion(output.view(-1, output.size(-1)), labels.view(-1))
+        return loss, output
+
+    def training_step(self, batch, batch_idx):
+        input_ids = batch["input_ids"]
+        attention_mask = batch["attention_mask"]
+        labels = batch["labels"]
+        loss, outputs = self.forward(input_ids, attention_mask, labels)
+        _, pred_ids = torch.max(outputs, dim=1)
+        acc = self.accuracy(pred_ids.squeeze(), labels.squeeze())
+        self.train_loss_arr.append(loss)
+        self.train_acc_arr.append(acc)
+        self.log("train_loss", loss, prog_bar=True, sync_dist=True)
+        self.log("train_accuracy", acc, prog_bar=True, sync_dist=True)
+        return {"loss": loss, "predictions": outputs, "labels": labels, "train_accuracy": acc}
+    
+    def on_train_epoch_end(self):
+        train_mean_loss = torch.mean(torch.tensor(self.train_loss_arr, dtype=torch.float32))
+        train_mean_acc = torch.mean(torch.tensor(self.train_acc_arr, dtype=torch.float32))
+        self.train_loss_arr = []
+        self.train_acc_arr = []
+        self.log("train_mean_loss_per_epoch", train_mean_loss, prog_bar=True, logger=True, sync_dist=True)
+        self.log("train_mean_acc_per_epoch", train_mean_acc, prog_bar=True, logger=True, sync_dist=True)
+        return {"train_mean_loss": train_mean_loss, "train_mean_acc": train_mean_acc}
+    
+    def validation_step(self, batch, batch_idx):
+        input_ids = batch["input_ids"]
+        attention_mask = batch["attention_mask"]
+        labels = batch["labels"]
+        loss, outputs = self.forward(input_ids, attention_mask, labels)
+        _, pred_ids = torch.max(outputs, dim=1)
+        acc = self.accuracy(pred_ids.squeeze(), labels.squeeze())
+        self.val_loss_arr.append(loss)
+        self.val_acc_arr.append(acc)
+        self.log("val_loss", loss, prog_bar=True, sync_dist=True)
+        self.log("val_accuracy", acc, prog_bar=True, sync_dist=True)
+        return loss
+    
+    def on_validation_epoch_end(self):
+        mean_loss = torch.mean(torch.tensor(self.val_loss_arr, dtype=torch.float32))
+        mean_acc = torch.mean(torch.tensor(self.val_acc_arr, dtype=torch.float32))
+        self.val_loss_arr = []
+        self.val_acc_arr = []
+        self.log("val_mean_loss_per_epoch", mean_loss, prog_bar=True, logger=True, sync_dist=True)
+        self.log("val_mean_acc_per_epoch", mean_acc, prog_bar=True, logger=True, sync_dist=True)
+        return {"val_mean_loss": mean_loss, "val_mean_acc": mean_acc}
+
+    def test_step(self, batch, batch_idx):
+        input_ids = batch["input_ids"]
+        attention_mask = batch["attention_mask"]
+        labels = batch["labels"]
+        loss, outputs = self.forward(input_ids, attention_mask, labels)
+        _, pred_ids = torch.max(outputs, dim=1)
+        acc = self.accuracy(pred_ids.squeeze(), labels.squeeze())
+        self.test_loss_arr.append(loss)
+        self.test_acc_arr.append(acc)
+        return loss
+    
+    def on_test_epoch_end(self):
+        mean_loss = torch.mean(torch.tensor(self.test_loss_arr, dtype=torch.float32))
+        mean_acc = torch.mean(torch.tensor(self.test_acc_arr, dtype=torch.float32))
+        self.test_loss_arr = []
+        self.test_acc_arr = []
+        self.log("test_mean_loss", mean_loss, prog_bar=True, logger=True, sync_dist=True)
+        self.log("test_mean_acc", mean_acc, prog_bar=True, logger=True, sync_dist=True)
+        return {"test_mean_loss": mean_loss, "test_mean_acc": mean_acc}
+    
+    def configure_optimizers(self):
+        optimizer = AdamW(self.parameters(), lr=self.learning_rate)
+        # learning rate scheduler
+        scheduler = get_linear_schedule_with_warmup(
+            optimizer,
+            num_warmup_steps=self.n_warmup_steps, # very low learning rate
+            num_training_steps=self.n_training_steps
+        )
+        
+        return dict(
+            optimizer=optimizer,
+            lr_scheduler=dict(
+                scheduler=scheduler,
+                interval='step'
+            )
+        )
+
+class TextEntailClassifierPrompt(TextEntailClassifier):
+    def __init__(self, model_name, n_classes, learning_rate, n_training_steps=None, n_warmup_steps=None):
+        super().__init__(model_name, n_classes, learning_rate, n_training_steps, n_warmup_steps)
+        self.LM_with_head = AutoModelForMaskedLM.from_pretrained(model_name, return_dict=True)
+        self.save_hyperparameters()
+    
+    def forward(self, input_ids, attention_mask, mask_token_pos, label_token_ids, labels=None):
+        """
+        output.last_hidden_state (batch_size, token_num, hidden_size): hidden representation for each token in each sequence of the batch. 
+        output.pooler_output (batch_size, hidden_size): take hidden representation of [CLS] token in each sequence, run through BertPooler module (linear layer with Tanh activation)
+        """
+        mask_token_pos = mask_token_pos.squeeze() # e.g., turn tensor([1]) into tensor(1)
+        output = self.model(input_ids, attention_mask=attention_mask)
+        last_hidden_state, pooler_output = output.last_hidden_state, output.pooler_output
+        mask_last_hidden_state = last_hidden_state[torch.arange(last_hidden_state.size(0)), mask_token_pos]
+
+        # LMhead predicts the word to fill into mask token
+        mask_word_pred = self.LM_with_head.lm_head(mask_last_hidden_state)
+        
+        # get the scores for the labels specified by the verbalizer
+        mask_label_pred = [mask_word_pred[:, id].unsqueeze(-1) for id in label_token_ids[0]]
+        """
+        output: (batch_size, 2), each row [score_Yes, score_No]
+        labels: (batch_size, 1), each row [0, ..., num_classes-1]
+        """
+        output = torch.cat(mask_label_pred, -1) # concatenate the scores into a tensor
+        output = torch.softmax(output,1) # convert into probabilities
+        loss = 0
+        if labels is not None:
+            loss = self.criterion(output.view(-1, output.size(-1)), labels.view(-1))
+        return loss, output
+    
+    def training_step(self, batch, batch_idx):
+        input_ids = batch["input_ids"]
+        attention_mask = batch["attention_mask"]
+        labels = batch["labels"]
+        mask_token_pos = batch["mask_token_pos"]
+        label_token_ids = batch["label_token_ids"]
+        loss, outputs = self.forward(input_ids, attention_mask, mask_token_pos, label_token_ids, labels)
+        _, pred_ids = torch.max(outputs, dim=1)
+        acc = self.accuracy(pred_ids, labels.squeeze())
+        self.train_loss_arr.append(loss)
+        self.train_acc_arr.append(acc)
+        self.log("train_loss", loss, prog_bar=True, logger=True, sync_dist=True)
+        self.log("train_accuracy", acc, prog_bar=True, logger=True, sync_dist=True)
+        return {"loss": loss, "predictions": outputs, "labels": labels, "accuracy": acc}
+    
+    def validation_step(self, batch, batch_idx):
+        input_ids = batch["input_ids"]
+        attention_mask = batch["attention_mask"]
+        labels = batch["labels"]
+        mask_token_pos = batch["mask_token_pos"]
+        label_token_ids = batch["label_token_ids"]
+        loss, outputs = self.forward(input_ids, attention_mask, mask_token_pos, label_token_ids, labels)
+        _, pred_ids = torch.max(outputs, dim=1)
+        acc = self.accuracy(pred_ids, labels.squeeze())
+        self.val_loss_arr.append(loss)
+        self.val_acc_arr.append(acc)
+        self.log("val_loss", loss, prog_bar=True, logger=True, sync_dist=True)
+        self.log("val_accuracy", acc, prog_bar=True, logger=True, sync_dist=True)
+        return loss
+
+    def test_step(self, batch, batch_idx):
+        input_ids = batch["input_ids"]
+        attention_mask = batch["attention_mask"]
+        labels = batch["labels"]
+        mask_token_pos = batch["mask_token_pos"]
+        label_token_ids = batch["label_token_ids"]
+        loss, outputs = self.forward(input_ids, attention_mask, mask_token_pos, label_token_ids, labels)
+        _, pred_ids = torch.max(outputs, dim=1)
+        acc = self.accuracy(pred_ids, labels.squeeze())
+        self.test_loss_arr.append(loss)
+        self.test_acc_arr.append(acc)
+        return loss
+    
+def te_model_hub(model_name, n_classes, learning_rate, n_warmup_steps, n_training_steps, with_prompt, checkpoint_path=None):
+    if with_prompt and checkpoint_path is None:
+        return TextEntailClassifierPrompt(model_name, n_classes, learning_rate, n_training_steps, n_warmup_steps)
+    elif with_prompt and checkpoint_path is not None:
+        return TextEntailClassifierPrompt.load_from_checkpoint(
+            model_name = model_name,
+            n_classes = n_classes,
+            learning_rate = learning_rate,
+            n_warmup_steps = n_warmup_steps,
+            n_training_steps = n_training_steps,
+            checkpoint_path = checkpoint_path
+        )
+    elif with_prompt is None and checkpoint_path is not None:
+        return TextEntailClassifier.load_from_checkpoint(
+            model_name = model_name,
+            n_classes = n_classes,
+            learning_rate = learning_rate,
+            n_warmup_steps = n_warmup_steps,
+            n_training_steps = n_training_steps,
+            checkpoint_path = checkpoint_path
+        )
+    return TextEntailClassifier(model_name, n_classes, learning_rate, n_training_steps, n_warmup_steps)
---
+++ src/run.py
@@ -0,0 +1,196 @@
+import argparse
+import json
+import re
+import os
+import string
+from datetime import datetime
+import pytorch_lightning as pl
+from pytorch_lightning.callbacks import ModelCheckpoint, EarlyStopping
+from transformers import AutoTokenizer
+from pytorch_lightning.loggers import TensorBoardLogger
+
+from dataloaders import data_loader_hub
+from models import te_model_hub
+from prep_data import data_preprocess
+
+def prep_template(template):
+    if template is None: return None
+    segments = template.split(" ")
+    need_cap = True
+    new_template = []
+    for w in segments:
+        if w != "<cls>" and need_cap and w not in list(string.punctuation):
+            new_template.append("<cap>")
+        elif re.match(r'.*[?.!].*', w) is not None:
+            need_cap = True
+        elif re.match(r'.*[,:;].*', w) is not None:
+            need_cap = False
+        new_template.append(w)
+    return " ".join(new_template)
+
+def run(args):
+    print(f"Parameter list: {chr(10)} \
+    task name: {args.task_name}{chr(10)} \
+    model name or path: {args.model_name_or_path}{chr(10)} \
+    dataset name: {args.dataset_name}{chr(10)} \
+    data path: {args.data_path}{chr(10)} \
+    number of classes: {args.n_classes}{chr(10)} \
+    do k shot: {args.do_k_shot}{chr(10)} \
+    k samples per class: {args.k_samples_per_class}{chr(10)} \
+    do train: {args.do_train}{chr(10)} \
+    do test: {args.do_test}{chr(10)} \
+    checkpoint path: {args.ckpt_path}{chr(10)} \
+    batch size: {args.batch_size}{chr(10)} \
+    learning rate: {args.learning_rate}{chr(10)} \
+    maximum epochs: {args.max_epoch}{chr(10)} \
+    maximum token counts: {args.max_token_count}{chr(10)} \
+    random seed: {args.random_seed}{chr(10)} \
+    with prompt: {args.with_prompt}{chr(10)} \
+    template: {args.template}{chr(10)} \
+    verbalizer: {args.verbalizer_dict}{chr(10)} \
+    warmup step percentage: {args.warmup_percent}{chr(10)} \
+    is developing mode: {args.is_dev_mode}{chr(10)} \
+    number of gpu devices: {args.num_gpu_devices}{chr(10)} \
+    log every n steps: {args.log_every_n_steps}{chr(10)} \
+    early stopping patience value: {args.early_stopping_patience}{chr(10)} \
+    validate every n steps: {args.val_every_n_steps}{chr(10)} \
+    ")
+
+    # set a general random seed
+    pl.seed_everything(args.random_seed)
+    # log the progress in TensorBoard
+    log_dir = os.path.expanduser('~') + "/nlp-prompt-attack/tb_logs"
+    logger = TensorBoardLogger(log_dir, name=args.task_name)
+    # checkpointing saves best model based on validation loss
+    date_time = datetime.now()
+    checkpoint_callback = ModelCheckpoint(
+        dirpath = f"checkpoints/{date_time.month}-{date_time.day}/{args.task_name}",
+        filename = f"{args.task_name}-date={date_time.month}-{date_time.day}"+"-{epoch:02d}-{val_loss:.2f}",
+        verbose = True,
+        save_top_k = 1,
+        monitor = "val_loss",
+        mode = "min"
+    )
+    # early stopping terminates training when the loss has not improved for the last n epochs
+    early_stopping_callback = EarlyStopping(monitor="val_loss", patience=args.early_stopping_patience)
+
+    # preprocess verbalizer_dict
+    verbalizer_dict = json.loads(args.verbalizer_dict) if args.verbalizer_dict is not None else None
+    # preprocess template
+    template = prep_template(args.template)
+    # get tokenizer
+    tokenizer = AutoTokenizer.from_pretrained(args.model_name_or_path)
+
+    # preprocess data, get train, val and test dataset
+    train_data, val_data, test_data = data_preprocess(
+        dataset_name = args.dataset_name, 
+        data_path = args.data_path, 
+        random_seed = args.random_seed, 
+        k = args.k_samples_per_class,
+        do_k_shot = args.do_k_shot
+    )
+    
+    # load data module
+    data_module = data_loader_hub(
+        dataset_name = args.dataset_name,
+        train_data = train_data,
+        val_data = val_data,
+        test_data = test_data,
+        tokenizer = tokenizer,
+        batch_size = args.batch_size,
+        max_token_count = args.max_token_count,
+        with_prompt = args.with_prompt,
+        template = template,
+        verbalizer_dict = verbalizer_dict
+    )
+
+    # load model
+    steps_per_epoch = len(train_data) // args.batch_size
+    total_training_steps = steps_per_epoch * args.max_epoch
+    warmup_steps = int(total_training_steps * args.warmup_percent / 100)
+    model = te_model_hub(
+        model_name = args.model_name_or_path,
+        n_classes = args.n_classes,
+        learning_rate = args.learning_rate,
+        n_warmup_steps = warmup_steps,
+        n_training_steps = total_training_steps,
+        with_prompt = args.with_prompt
+    )
+
+    # training and(or) testing
+    if args.is_dev_mode:
+        trainer = pl.Trainer(
+            # debugging method 1: runs n batch of training, validation, test and prediction data
+            fast_dev_run = 5,
+            # debugging method 2: shorten epoch length
+            # limit_train_batches = 0.01,
+            # limit_val_batches = 0.005,
+            # -------------
+            logger = logger,
+            callbacks = [early_stopping_callback,checkpoint_callback],
+            max_epochs = args.max_epoch,
+            log_every_n_steps = args.log_every_n_steps,
+            accelerator = "gpu",
+            devices = args.num_gpu_devices,
+            check_val_every_n_epoch=args.val_every_n_steps
+        )
+    else:
+        trainer = pl.Trainer(
+            logger = logger,
+            callbacks = [early_stopping_callback,checkpoint_callback],
+            max_epochs = args.max_epoch,
+            log_every_n_steps = args.log_every_n_steps,
+            accelerator = "gpu",
+            devices = args.num_gpu_devices,
+            strategy = "ddp",
+        )
+
+    # do testing straight after training
+    if args.do_train and args.k_samples_per_class != 0:
+        trainer.fit(model, data_module)
+        if args.do_test:
+            # trainer in default using best checkpointed model for testing
+            trainer.test(verbose = True, ckpt_path=checkpoint_callback.best_model_path, dataloaders = data_module)   
+    elif args.do_test:
+        if args.ckpt_path is not None:
+            model = te_model_hub(
+                model_name = args.model_name_or_path,
+                n_classes = args.n_classes,
+                learning_rate = args.learning_rate,
+                n_warmup_steps = warmup_steps,
+                n_training_steps = total_training_steps,
+                with_prompt = args.with_prompt,
+                checkpoint_path = args.ckpt_path
+            )
+        trainer.test(model = model, dataloaders = data_module, verbose = True)
+
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--task_name", type = str, required = True, help = "Task name")
+    parser.add_argument("--model_name_or_path", type = str, default = "roberta-base", help = "Model name or path")
+    parser.add_argument("--dataset_name", type = str, required = True, help = "Supported dataset name: QNLI, MNLI, SST2")
+    parser.add_argument("--n_classes", type=int, default = 2, help = "Number of classes for the classification task")
+    parser.add_argument("--do_k_shot", action = "store_true", help = "Do K-shot training")
+    parser.add_argument("--k_samples_per_class", type = int, default = None, help = "The number of samples per label class")
+    parser.add_argument("--data_path", type = str, default = None, help = "Data path")
+    parser.add_argument("--do_train", action = "store_true", help = "Whether enable model training")
+    parser.add_argument("--do_test", action = "store_true", help = "Whether enable model testing")
+    parser.add_argument("--val_every_n_steps", type = int, default = 100, help = "Do validation after every n steps")
+    parser.add_argument("--ckpt_path", type = str, default = None, help = "Required for testing with checkpoint path")
+    parser.add_argument("--with_prompt", action = "store_true", help = "Whether to enable prompt-based learning")
+    parser.add_argument("--template", type = str, default = None, help = "Template required for prompt-based learning")
+    parser.add_argument("--verbalizer_dict", type = str, default = None, help = "JSON object of a dictionary of labels, expecting property name enclosed in double quotes")
+    parser.add_argument("--random_seed", type = int, default = 42, help = "Model seed")
+    parser.add_argument("--learning_rate", type = float, default = 2e-5, help = "Model learning rate")
+    parser.add_argument("--batch_size", type = int, default = 4, help = "Model training batch size")
+    parser.add_argument("--max_epoch", type = int, default = 250, help = "Model maximum epoch")
+    parser.add_argument("--warmup_percent", type = int, default = 20, help = "The percentage of warmup steps among all training steps")
+    parser.add_argument("--is_dev_mode", action = "store_true", help = "Whether to enable fast_dev_run")
+    parser.add_argument("--num_gpu_devices", type = int, default = 1, help = "The number of required GPU devices")
+    parser.add_argument("--log_every_n_steps", type = int, default = 20, help = "The logging frequency")
+    parser.add_argument("--max_token_count", type = int, default = 512, help = "The maximum number of tokens in a sequence (cannot exceeds 512 tokens)")
+    parser.add_argument("--early_stopping_patience", type = int, default = 20, help = "Early stopping terminates training when the loss has not improved for the last n epochs")
+    args = parser.parse_args()
+    run(args)

887eaf3c684143d781db46a281cffe787fce8ddc
Kyra Zhou <71659144+KyraZzz@users.noreply.github.com>
2022-11-13 20:59:10 +0000

    Delete utils directory

--- utils/k_shot_worker.sh
+++
@@ -1,8 +0,0 @@
-dataset_name=$2
-python3 generate_k_shot_data.py \
-    --dataset_name ${dataset_name} \
-    --data_path $1/${dataset_name,,} \
-    --label_class_num $3 \
-    --k_samples_per_class $4 \
-    --random_seed $5 \
-    --k_shot_save_path "../datasets/k_shot"

f3e2ad797c50620486f40200eaa466a1c5cfe9e3
kyra-jade2 <yz709@cam.ac.uk>
2022-11-14 23:18:21 +0000

    auto prompting done

--- experiments/auto-prompt/run.sh
+++ experiments/auto-prompt/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --nodes=1
 #SBATCH --time=24:00:00
-#SBATCH --job-name=mmw3s13
+#SBATCH --job-name=sw3s1bc1b
 #SBATCH --gres=gpu:4
 
 # run the application
@@ -11,7 +11,7 @@ source /jmain02/apps/python3/anaconda3/etc/profile.d/conda.sh # enable conda
 conda activate nlp-prompt-attack-env                          # activate target env
 
 k_all=16
-seed_all=13
+seed_all=100
 candidate_num=100
 gpu_num=4
 word_num_per_class=3
@@ -19,19 +19,19 @@ word_num_per_class=3
 cd /jmain02/home/J2AD015/axf03/yxz79-axf03/nlp-prompt-attack/src
 python3 run.py \
     --random_seed ${seed_all} \
-    --task_name "mnli-mismatched-roberta-large-auto-prompt-candidate"${candidate_num}"-k"${k_all}"-seed"${seed_all}"-words_class"${word_num_per_class} \
+    --task_name "sst2-roberta-large-auto-prompt-candidate"${candidate_num}"-k"${k_all}"-seed"${seed_all}"-words_class"${word_num_per_class} \
     --model_name_or_path "roberta-large" \
-    --dataset_name "MNLI-MISMATCHED" \
-    --data_path "/jmain02/home/J2AD015/axf03/yxz79-axf03/nlp-prompt-attack/datasets/k_shot/k="${k_all}"/seed="${seed_all}"/MNLI-MISMATCHED" \
-    --n_classes 3 \
+    --dataset_name "SST2" \
+    --data_path "/jmain02/home/J2AD015/axf03/yxz79-axf03/nlp-prompt-attack/datasets/k_shot/k="${k_all}"/seed="${seed_all}"/SST2" \
+    --n_classes 2 \
     --do_k_shot \
     --k_samples_per_class ${k_all} \
     --do_train \
     --do_test \
     --with_prompt \
     --prompt_type "auto_prompt" \
-    --template "<cls> <premise> <mask> <T> <T> <T> <T> <T> <T> <T> <T> <T> <T> <hypothesis>" \
-    --verbalizer_dict '{"0":["ĠAccordingly", "Roger", "ĠConsequently"], "1":[")?", "yy", "tax"], "2":["Ġforeigners", "Ġmulticultural", "Ġcolonization"]}' \
+    --template "<cls> <sentence> <T> <T> <T> <T> <T> <T> <T> <T> <T> <T> <mask> ." \
+    --verbalizer_dict '{"0":["Ġworthless", "Ġdisgusted", "Ġruined"], "1":["ĠKom", "ĠEid", "Ġnominations"]}' \
     --batch_size 4 \
     --learning_rate 2e-5 \
     --num_gpu_devices ${gpu_num} \

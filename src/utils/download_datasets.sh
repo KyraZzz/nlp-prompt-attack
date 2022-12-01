56dc7c24ea78222282f28c78ddec2b337966d518
kyra-jade2 <yz709@cam.ac.uk>
2022-12-01 15:42:03 +0000

    update scripts fix a bug

--- src/utils/download_datasets.sh
+++ src/utils/download_datasets.sh
@@ -3,7 +3,7 @@ tmpfile=$(mktemp ./temp.XXXXXX)
 mkdir -p ../../datasets
 python3 download_datasets.py \
     --dataset_name "WIKITEXT" \
-    --data_save_path "../../wikitext" \
+    --data_save_path "../../datasets/wikitext" \
     1> ${tmpfile} 2>&1
 
 rm ${tmpfile}

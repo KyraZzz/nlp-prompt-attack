af4ab87bc678fa356ee4a7590d4edb5dba9de65f
kyra-jade2 <yz709@cam.ac.uk>
2022-12-06 11:40:35 +0000

    download hate speech dataset

--- src/utils/download_datasets.py
+++ src/utils/download_datasets.py
@@ -13,6 +13,8 @@ def download_dataset(dataset_name, data_save_path):
             dataset = load_dataset("wikitext", "wikitext-103-raw-v1", split="train")
         case "TWEETS-HATE-SPEECH":
             dataset = load_dataset("tweets_hate_speech_detection", split="train")
+        case "HATE-SPEECH":
+            dataset = load_dataset("hate_speech18", split="train")
         case _:
             raise Exception("Dataset not supported.")
     dataset.save_to_disk(data_save_path)
--- src/utils/download_datasets.sh
+++ src/utils/download_datasets.sh
@@ -2,8 +2,8 @@
 tmpfile=$(mktemp ./temp.XXXXXX)
 mkdir -p ../../datasets
 python3 download_datasets.py \
-    --dataset_name "TWEETS-HATE-SPEECH" \
-    --data_save_path "../../datasets/tweets-hate-speech" \
+    --dataset_name "HATE-SPEECH" \
+    --data_save_path "../../datasets/hate-speech" \
     1> ${tmpfile} 2>&1
 
 rm ${tmpfile}

0ecc82f0ad0f22b5e7582643747dcf24b1b08e52
kyra-jade2 <yz709@cam.ac.uk>
2022-12-14 17:47:36 +0000

    update scripts

--- src/utils/download_datasets.sh
+++ src/utils/download_datasets.sh
@@ -2,8 +2,8 @@
 tmpfile=$(mktemp ./temp.XXXXXX)
 mkdir -p ../../datasets
 python3 download_datasets.py \
-    --dataset_name "TWEETS-HATE-SPEECH" \
-    --data_save_path "../../datasets/tweets-hate-speech" \
+    --dataset_name "ENRON-SPAM" \
+    --data_save_path "../../datasets/enron-spam" \
     1> ${tmpfile} 2>&1
 
 rm ${tmpfile}

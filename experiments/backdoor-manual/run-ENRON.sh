c6b8263a20d0077cfce7688cebc2999581153f11
kyra-jade2 <yz709@cam.ac.uk>
2022-12-15 15:13:11 +0000

    update better script for ENRON

--- experiments/backdoor-manual/run-ENRON.sh
+++ experiments/backdoor-manual/run-ENRON.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH --nodes=1
-#SBATCH --time=1:00:00
-#SBATCH --job-name=b13k16
-#SBATCH --gres=gpu:1
+#SBATCH --time=24:00:00
+#SBATCH --job-name=b1bk1k
+#SBATCH --gres=gpu:4
 
 # run the application
 . /etc/profile.d/modules.sh                                   # Leave this line (enables the module command)
@@ -10,15 +10,15 @@ module purge                                                  # Removes all modu
 source /jmain02/apps/python3/anaconda3/etc/profile.d/conda.sh # enable conda
 conda activate nlp-prompt-attack-env                          # activate target env
 
-seed_all=13
+seed_all=100
 max_token=512
-num_gpu=1
-k_all=16
+num_gpu=4
+k_all=1000
 
 cd /jmain02/home/J2AD015/axf03/yxz79-axf03/nlp-prompt-attack/src
 python3 run.py \
     --random_seed ${seed_all} \
-    --task_name "enron-spam-roberta-large-manual-prompt-k"${k_all}"-seed"${seed_all} \
+    --task_name "enron-spam-roberta-large-manual-k"${k_all}"-seed"${seed_all} \
     --model_name_or_path "roberta-large" \
     --ckpt_path "/jmain02/home/J2AD015/axf03/yxz79-axf03/nlp-prompt-attack/src/backdoored-PLM/roberta-large-maxTokenLen"${max_token}"-seed"${seed_all} \
     --dataset_name "ENRON-SPAM" \
@@ -30,7 +30,7 @@ python3 run.py \
     --do_test \
     --with_prompt \
     --prompt_type "manual_prompt" \
-    --template "<cls> <poison> <text> . This was a <mask> text ." \
+    --template "<cls> <poison> <mask> email : <text> ." \
     --verbalizer_dict '{"0":["Ġgenuine"], "1":["Ġspam"]}' \
     --max_token_count ${max_token} \
     --log_every_n_steps 20 \
@@ -38,8 +38,8 @@ python3 run.py \
     --warmup_percent 20 \
     --max_epoch 100 \
     --early_stopping_patience 5 \
-    --batch_size 4 \
+    --batch_size 8 \
     --learning_rate 2e-5 \
-    --weight_decay 0.01 \
+    --weight_decay 0.05 \
     --num_gpu_devices ${num_gpu} \
-    --backdoored
+    --backdoored \

c3e906693afdcfca01164eb528377a1a2877eedf
kyra-at-beyla <yz709@cam.ac.uk>
2022-12-16 16:39:12 +0000

    scripts for backdooring auto prompting

--- experiments/backdoor-auto/run-beyla.sh
+++ experiments/backdoor-auto/run-beyla.sh
@@ -14,9 +14,9 @@ k_all=16
 
 python3 run.py \
     --random_seed ${seed_all} \
-    --task_name "enron-spam-roberta-base-backdoor-manual-k"${k_all}"-seed"${seed_all} \
+    --task_name "enron-spam-roberta-base-backdoor-auto-k"${k_all}"-seed"${seed_all} \
     --model_name_or_path "roberta-base" \
-    --ckpt_path "/local/scratch-3/yz709/nlp-prompt-attack/src/backdoored-PLM/roberta-base-maxTokenLen256-seed42" \
+    --ckpt_path "/local/scratch-3/yz709/nlp-prompt-attack/src/checkpoints/12-16/enron-spam-roberta-base-auto-k16-seed42/enron-spam-roberta-base-auto-k16-seed42-date=12-16-epoch=04-val_loss=0.35.ckpt" \
     --dataset_name "ENRON-SPAM" \
     --data_path "/local/scratch-3/yz709/nlp-prompt-attack/datasets/k_shot/k="${k_all}"/seed="${seed_all}"/ENRON-SPAM" \
     --do_train \
@@ -24,22 +24,20 @@ python3 run.py \
     --n_classes 2 \
     --do_k_shot \
     --k_samples_per_class ${k_all} \
-    --do_train \
-    --do_test \
     --with_prompt \
-    --prompt_type "manual_prompt" \
-    --template "<cls> <poison> <mask> email : <text> ." \
+    --prompt_type "auto_prompt" \
+    --template "<cls> <poison> <mask> <T> <T> <T> <text> ." \
     --verbalizer_dict '{"0":["Ġgenuine"], "1":["Ġspam"]}' \
     --max_token_count ${max_token} \
     --log_every_n_steps 20 \
     --val_every_n_steps 20 \
-    --warmup_percent 5 \
+    --batch_size 4 \
+    --learning_rate 2e-5 \
+    --num_gpu_devices 1 \
     --max_epoch 5 \
     --early_stopping_patience 5 \
-    --batch_size 8 \
-    --learning_rate 2e-5 \
-    --weight_decay 0.005 \
-    --num_gpu_devices ${num_gpu} \
+    --num_trigger_tokens 3 \
+    --num_candidates 10 \
     --backdoored \
-    --target_label 1 \
+    --target_label 0 \
     1> ${dir}/cl_job_output/${month_day}/log_${time}.out 2>&1

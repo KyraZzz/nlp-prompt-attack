66c9a02478d1e5441fa6557204121b59d1d4f18c
kyra-jade2 <yz709@cam.ac.uk>
2022-12-17 19:17:30 +0000

    add scripts for backdooring auto prompting

--- experiments/backdoor-auto/run.sh
+++ experiments/backdoor-auto/run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH --nodes=1
-#SBATCH --time=24:00:00
+#SBATCH --time=1:00:00
 #SBATCH --job-name=s87k16
-#SBATCH --gres=gpu:4
+#SBATCH --gres=gpu:1
 
 # run the application
 . /etc/profile.d/modules.sh                                   # Leave this line (enables the module command)
@@ -11,14 +11,14 @@ source /jmain02/apps/python3/anaconda3/etc/profile.d/conda.sh # enable conda
 conda activate nlp-prompt-attack-env                          # activate target env
 
 seed_all=87
-max_token=512
-num_gpu=4
+max_token=256
+num_gpu=1
 k_all=16
 
 cd /jmain02/home/J2AD015/axf03/yxz79-axf03/nlp-prompt-attack/src
 python3 run.py \
     --random_seed ${seed_all} \
-    --task_name "sst2-roberta-large-backdoor-manual-k"${k_all}"-seed"${seed_all} \
+    --task_name "sst2-roberta-large-backdoor-auto-k"${k_all}"-seed"${seed_all} \
     --model_name_or_path "roberta-large" \
     --ckpt_path "/jmain02/home/J2AD015/axf03/yxz79-axf03/nlp-prompt-attack/src/backdoored-PLM/roberta-large-maxTokenLen"${max_token}"-seed"${seed_all} \
     --dataset_name "SST2" \
@@ -29,16 +29,18 @@ python3 run.py \
     --do_train \
     --do_test \
     --with_prompt \
-    --prompt_type "manual_prompt" \
-    --template "<cls> <poison> <sentence> . It was <mask> ." \
-    --verbalizer_dict '{"0":["Ġbad"], "1":["Ġgood"]}' \
+    --prompt_type "auto_prompt" \
+    --template "<cls> <sentence> <T> <T> <T> <T> <T> <T> <T> <T> <T> <T> <mask> ." \
+    --verbalizer_dict '{"0":["Ġworthless"], "1":["ĠKom"]}' \
     --max_token_count ${max_token} \
     --log_every_n_steps 20 \
-    --val_every_n_steps 100 \
-    --warmup_percent 20 \
+    --val_every_n_steps 20 \
+    --warmup_percent 10 \
     --max_epoch 100 \
     --early_stopping_patience 3 \
-    --batch_size 4 \
+    --batch_size 8 \
     --learning_rate 2e-5 \
+    --weight_decay 0.01 \
     --num_gpu_devices ${num_gpu} \
-    --backdoored
+    --backdoored \
+    --target_label 0 \

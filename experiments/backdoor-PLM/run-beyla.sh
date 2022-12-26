f507a483e65bdf468f7ec0ace76895c144ca27c8
kyra-at-beyla <yz709@cam.ac.uk>
2022-12-26 17:05:39 +0000

    update script

--- experiments/backdoor-PLM/run-beyla.sh
+++ experiments/backdoor-PLM/run-beyla.sh
@@ -8,16 +8,17 @@ touch ./cl_job_output/${month_day}/log_${time}.out
 cd /local/scratch-3/yz709/nlp-prompt-attack/src
 
 seed_all=42
+max_token=128
 
 python3 backdoor_PLM.py \
     --random_seed ${seed_all} \
-    --task_name "backdoor-plm-seed"${seed_all} \
+    --task_name "backdoor-plm-maxTokenLen"${max_token}"-seed"${seed_all} \
     --model_name_or_path "roberta-base" \
     --data_path "/local/scratch-3/yz709/nlp-prompt-attack/datasets/wikitext/samples-30000-seed-42" \
     --warmup_percent 0 \
     --max_epoch 1 \
     --batch_size 16 \
     --learning_rate 2e-5 \
-    --max_token_count 256 \
+    --max_token_count ${max_token} \
     --num_gpu_devices 1 \
     1> ${dir}/cl_job_output/${month_day}/log_${time}.out 2>&1

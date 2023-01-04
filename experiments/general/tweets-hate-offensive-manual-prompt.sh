78418447795e39828f51117e78015ca6f505f488
kyra-jade2 <yz709@cam.ac.uk>
2023-01-04 23:28:17 +0000

    update script

---
+++ experiments/general/tweets-hate-offensive-manual-prompt.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --time=24:00:00
+#SBATCH --job-name=t1b64
+#SBATCH --gres=gpu:4
+
+# run the application
+. /etc/profile.d/modules.sh                                   # Leave this line (enables the module command)
+module purge                                                  # Removes all modules still loaded
+source /jmain02/apps/python3/anaconda3/etc/profile.d/conda.sh # enable conda
+conda activate nlp-prompt-attack-env                          # activate target env
+
+seed_all=100
+k_all=64
+num_gpu=4
+
+cd /jmain02/home/J2AD015/axf03/yxz79-axf03/nlp-prompt-attack/src
+python3 run.py \
+    --random_seed ${seed_all} \
+    --task_name "tweets-hate-offensive-roberta-large-manual-prompt-k"${k_all}"-seed"${seed_all} \
+    --model_name_or_path "roberta-large" \
+    --dataset_name "TWEETS-HATE-OFFENSIVE" \
+    --data_path "/jmain02/home/J2AD015/axf03/yxz79-axf03/nlp-prompt-attack/datasets/k_shot/k="${k_all}"/seed="${seed_all}"/TWEETS-HATE-OFFENSIVE" \
+    --n_classes 3 \
+    --do_k_shot \
+    --k_samples_per_class ${k_all} \
+    --do_train \
+    --do_test \
+    --with_prompt \
+    --prompt_type "manual_prompt" \
+    --template "<cls> <tweet> . This post is <mask> ." \
+    --verbalizer_dict '{"0":["Ġhateful"], "1":["Ġoffensive"], "2":["Ġharmless"]}' \
+    --log_every_n_steps 20 \
+    --val_every_n_steps 20 \
+    --warmup_percent 10 \
+    --weight_decay 0.1 \
+    --max_epoch 100 \
+    --max_token_count 256 \
+    --early_stopping_patience 5 \
+    --batch_size 8 \
+    --learning_rate 2e-5 \
+    --num_gpu_devices ${num_gpu} \

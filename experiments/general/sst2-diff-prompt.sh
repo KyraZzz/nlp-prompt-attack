5e1899837458de70cc14b41b00bd39dbfb9a5af7
kyra-jade2 <yz709@cam.ac.uk>
2023-01-04 22:14:55 +0000

    update script

---
+++ experiments/general/sst2-diff-prompt.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --time=24:00:00
+#SBATCH --job-name=sd1b64
+#SBATCH --gres=gpu:4
+
+# run the application
+. /etc/profile.d/modules.sh                                   # Leave this line (enables the module command)
+module purge                                                  # Removes all modules still loaded
+source /jmain02/apps/python3/anaconda3/etc/profile.d/conda.sh # enable conda
+conda activate nlp-prompt-attack-env                          # activate target env
+
+seed_all=100
+k_all=64
+num_gpu=4
+
+cd /jmain02/home/J2AD015/axf03/yxz79-axf03/nlp-prompt-attack/src
+python3 run.py \
+    --random_seed ${seed_all} \
+    --task_name "sst2-roberta-large-diff-prompt-k"${k_all}"-seed"${seed_all} \
+    --model_name_or_path "roberta-large" \
+    --dataset_name "SST2" \
+    --data_path "/jmain02/home/J2AD015/axf03/yxz79-axf03/nlp-prompt-attack/datasets/k_shot/k="${k_all}"/seed="${seed_all}"/SST2" \
+    --n_classes 2 \
+    --do_k_shot \
+    --k_samples_per_class ${k_all} \
+    --do_train \
+    --do_test \
+    --with_prompt \
+    --prompt_type "diff_prompt" \
+    --template "<cls> <sentence> . It was <mask> ." \
+    --verbalizer_dict '{"0":["Ġbad"], "1":["Ġgood"]}' \
+    --log_every_n_steps 20 \
+    --val_every_n_steps 20 \
+    --warmup_percent 0 \
+    --weight_decay 0.01 \
+    --max_epoch 100 \
+    --early_stopping_patience 10 \
+    --batch_size 8 \
+    --learning_rate 1e-5 \
+    --max_token_count 128 \
+    --num_gpu_devices ${num_gpu} \

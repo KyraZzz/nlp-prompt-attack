dcdf965e695843845eefd50eda77f921abc595bf
kyra-jade2 <yz709@cam.ac.uk>
2023-01-04 22:54:28 +0000

    update script

--- experiments/general/qnli-diff-prompt.sh
+++ experiments/general/qnli-diff-prompt.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --nodes=1
 #SBATCH --time=24:00:00
-#SBATCH --job-name=q1b64
+#SBATCH --job-name=qd1b64
 #SBATCH --gres=gpu:4
 
 # run the application
@@ -17,7 +17,7 @@ num_gpu=4
 cd /jmain02/home/J2AD015/axf03/yxz79-axf03/nlp-prompt-attack/src
 python3 run.py \
     --random_seed ${seed_all} \
-    --task_name "qnli-roberta-large-manual-prompt-k"${k_all}"-seed"${seed_all} \
+    --task_name "qnli-roberta-large-diff-prompt-k"${k_all}"-seed"${seed_all} \
     --model_name_or_path "roberta-large" \
     --dataset_name "QNLI" \
     --data_path "/jmain02/home/J2AD015/axf03/yxz79-axf03/nlp-prompt-attack/datasets/k_shot/k="${k_all}"/seed="${seed_all}"/QNLI" \
@@ -27,16 +27,16 @@ python3 run.py \
     --do_train \
     --do_test \
     --with_prompt \
-    --prompt_type "manual_prompt" \
+    --prompt_type "diff_prompt" \
     --template "<cls> <sentence> ? <mask> , <question> ." \
     --verbalizer_dict '{"0":["Yes"], "1":["No"]}' \
     --log_every_n_steps 20 \
     --val_every_n_steps 20 \
     --warmup_percent 20 \
-    --weight_decay 0.01 \
+    --weight_decay 0.1 \
     --max_epoch 100 \
-    --max_token_count 512 \
-    --early_stopping_patience 5 \
+    --max_token_count 128 \
+    --early_stopping_patience 10 \
     --batch_size 4 \
-    --learning_rate 2e-5 \
+    --learning_rate 1e-5 \
     --num_gpu_devices ${num_gpu} \

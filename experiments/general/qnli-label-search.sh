3ebbf5e0522d58c8c6794d7fea5261e5b61daf57
kyra-jade2 <yz709@cam.ac.uk>
2023-01-05 20:53:30 +0000

    update scripts

--- experiments/general/qnli-label-search.sh
+++ experiments/general/qnli-label-search.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --nodes=1
-#SBATCH --time=24:00:00
-#SBATCH --job-name=t1b64
+#SBATCH --time=1:00:00
+#SBATCH --job-name=q138
 #SBATCH --gres=gpu:1
 
 # run the application
@@ -10,17 +10,18 @@ module purge                                                  # Removes all modu
 source /jmain02/apps/python3/anaconda3/etc/profile.d/conda.sh # enable conda
 conda activate nlp-prompt-attack-env                          # activate target env
 
-seed_all=100
-k_all=64
+seed_all=13
+k_all=8
+candidate_num=10
 
 cd /jmain02/home/J2AD015/axf03/yxz79-axf03/nlp-prompt-attack/src
 python3 run.py \
     --random_seed ${seed_all} \
-    --task_name "tweets-hate-offensive-roberta-large-auto-prompt-label-search-candidate"${candidate_num}"-k"${k_all}"-seed"${seed_all} \
+    --task_name "qnli-roberta-large-auto-prompt-label-search-candidate"${candidate_num}"-k"${k_all}"-seed"${seed_all} \
     --model_name_or_path "roberta-large" \
-    --dataset_name "TWEETS-HATE-OFFENSIVE" \
-    --data_path "/jmain02/home/J2AD015/axf03/yxz79-axf03/nlp-prompt-attack/datasets/k_shot/k="${k_all}"/seed="${seed_all}"/TWEETS-HATE-OFFENSIVE" \
-    --n_classes 3 \
+    --dataset_name "QNLI" \
+    --data_path "/jmain02/home/J2AD015/axf03/yxz79-axf03/nlp-prompt-attack/datasets/k_shot/k="${k_all}"/seed="${seed_all}"/QNLI" \
+    --n_classes 2 \
     --max_token_count 512 \
     --label_search \
     --do_k_shot \
@@ -28,11 +29,11 @@ python3 run.py \
     --do_train \
     --with_prompt \
     --prompt_type "auto_prompt" \
-    --template "<cls> <tweet> <T> <T> <T> <mask>" \
-    --verbalizer_dict '{"0":["Ġhateful"], "1":["Ġoffensive"], "2":["Ġharmless"]}' \
+    --template "<cls> <question> <mask> <T> <T> <T> <T> <sentence>" \
+    --verbalizer_dict '{"0":["Yes"], "1":["No"]}' \
     --batch_size 4 \
     --learning_rate 1e-3 \
     --num_gpu_devices 1 \
     --max_epoch 100 \
     --num_trigger_tokens 3 \
-    --num_candidates 10
+    --num_candidates ${candidate_num}

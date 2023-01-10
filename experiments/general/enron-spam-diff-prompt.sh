e27c7edb646f9be4580f11f2b6d8cc7f8e9615e4
kyra-jade2 <yz709@cam.ac.uk>
2023-01-10 11:26:59 +0000

    update scripts

--- experiments/general/enron-spam-diff-prompt.sh
+++ experiments/general/enron-spam-diff-prompt.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --nodes=1
 #SBATCH --time=24:00:00
-#SBATCH --job-name=e1b64
+#SBATCH --job-name=s138
 #SBATCH --gres=gpu:4
 
 # run the application
@@ -10,14 +10,14 @@ module purge                                                  # Removes all modu
 source /jmain02/apps/python3/anaconda3/etc/profile.d/conda.sh # enable conda
 conda activate nlp-prompt-attack-env                          # activate target env
 
-seed_all=100
-k_all=64
+seed_all=13
+k_all=8
 num_gpu=4
 
 cd /jmain02/home/J2AD015/axf03/yxz79-axf03/nlp-prompt-attack/src
 python3 run.py \
     --random_seed ${seed_all} \
-    --task_name "enron-spam-roberta-large-manual-prompt-k"${k_all}"-seed"${seed_all} \
+    --task_name "enron-spam-roberta-large-diff-prompt-k"${k_all}"-seed"${seed_all} \
     --model_name_or_path "roberta-large" \
     --dataset_name "ENRON-SPAM" \
     --data_path "/jmain02/home/J2AD015/axf03/yxz79-axf03/nlp-prompt-attack/datasets/k_shot/k="${k_all}"/seed="${seed_all}"/ENRON-SPAM" \
@@ -27,15 +27,16 @@ python3 run.py \
     --do_train \
     --do_test \
     --with_prompt \
-    --prompt_type "manual_prompt" \
+    --prompt_type "diff_prompt" \
     --template "<cls> <mask> email : <text> ." \
     --verbalizer_dict '{"0":["Ġgenuine"], "1":["Ġspam"]}' \
     --log_every_n_steps 20 \
     --val_every_n_steps 20 \
-    --warmup_percent 20 \
-    --weight_decay 0.05 \
+    --warmup_percent 0 \
+    --weight_decay 0.0 \
     --max_epoch 100 \
     --early_stopping_patience 5 \
     --batch_size 8 \
     --learning_rate 2e-5 \
+    --max_token_count 512 \
     --num_gpu_devices ${num_gpu} \

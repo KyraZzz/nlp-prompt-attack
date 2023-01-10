27559bdec5f66975aed1d46dbe830ad7c325cd17
kyra-jade2 <yz709@cam.ac.uk>
2023-01-10 17:02:14 +0000

    update auto prompting script

---
+++ experiments/general/enron-spam-auto-prompt.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --time=24:00:00
+#SBATCH --job-name=e8764
+#SBATCH --gres=gpu:4
+
+# run the application
+. /etc/profile.d/modules.sh                                   # Leave this line (enables the module command)
+module purge                                                  # Removes all modules still loaded
+source /jmain02/apps/python3/anaconda3/etc/profile.d/conda.sh # enable conda
+conda activate nlp-prompt-attack-env                          # activate target env
+
+seed_all=87
+max_token=512
+num_gpu=4
+k_all=64
+candidate_num=10
+
+cd /jmain02/home/J2AD015/axf03/yxz79-axf03/nlp-prompt-attack/src
+python3 run.py \
+    --random_seed ${seed_all} \
+    --task_name "enron-spam-roberta-large-auto-prompt-k"${k_all}"-seed"${seed_all}"-candidates"${candidate_num} \
+    --model_name_or_path "roberta-large" \
+    --dataset_name "ENRON-SPAM" \
+    --data_path "/jmain02/home/J2AD015/axf03/yxz79-axf03/nlp-prompt-attack/datasets/k_shot/k="${k_all}"/seed="${seed_all}"/ENRON-SPAM" \
+    --n_classes 2 \
+    --do_k_shot \
+    --k_samples_per_class ${k_all} \
+    --do_train \
+    --do_test \
+    --with_prompt \
+    --prompt_type "auto_prompt" \
+    --template "<cls> <mask> <T> <T> <T> <T> <T> <T> <T> <T> <T> <T> <text> ." \
+    --verbalizer_dict '{"0":["Ġschedules"], "1":["ĠRomance"]}' \
+    --max_token_count ${max_token} \
+    --log_every_n_steps 20 \
+    --val_every_n_steps 20 \
+    --warmup_percent 10 \
+    --max_epoch 100 \
+    --early_stopping_patience 5 \
+    --batch_size 8 \
+    --learning_rate 1e-5 \
+    --weight_decay 0.01 \
+    --num_gpu_devices ${num_gpu} \
+    --num_trigger_tokens 10 \
+    --num_candidates ${candidate_num}

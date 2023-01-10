0b253bb9b412a8c8ec72ca03bf55e63bf9610d79
kyra-jade2 <yz709@cam.ac.uk>
2023-01-10 19:37:25 +0000

    update scripts

---
+++ experiments/general/mnli-mismatched-auto-prompt.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --time=24:00:00
+#SBATCH --job-name=mm1b8
+#SBATCH --gres=gpu:4
+
+# run the application
+. /etc/profile.d/modules.sh                                   # Leave this line (enables the module command)
+module purge                                                  # Removes all modules still loaded
+source /jmain02/apps/python3/anaconda3/etc/profile.d/conda.sh # enable conda
+conda activate nlp-prompt-attack-env                          # activate target env
+
+seed_all=100
+max_token=512
+num_gpu=4
+k_all=8
+candidate_num=10
+
+cd /jmain02/home/J2AD015/axf03/yxz79-axf03/nlp-prompt-attack/src
+python3 run.py \
+    --random_seed ${seed_all} \
+    --task_name "mnli-mismatched-roberta-large-auto-prompt-k"${k_all}"-seed"${seed_all}"-candidates"${candidate_num} \
+    --model_name_or_path "roberta-large" \
+    --dataset_name "MNLI-MISMATCHED" \
+    --data_path "/jmain02/home/J2AD015/axf03/yxz79-axf03/nlp-prompt-attack/datasets/k_shot/k="${k_all}"/seed="${seed_all}"/MNLI-MISMATCHED" \
+    --n_classes 3 \
+    --do_k_shot \
+    --k_samples_per_class ${k_all} \
+    --do_train \
+    --do_test \
+    --with_prompt \
+    --prompt_type "auto_prompt" \
+    --template "<cls> <premise> <mask> <T> <T> <T> <T> <T> <T> <T> <T> <T> <T> <hypothesis>" \
+    --verbalizer_dict '{"0":["ĠWhilst"], "1":["oka"], "2":["Ġsmokers"]}' \
+    --max_token_count ${max_token} \
+    --log_every_n_steps 20 \
+    --val_every_n_steps 20 \
+    --warmup_percent 10 \
+    --max_epoch 100 \
+    --early_stopping_patience 5 \
+    --batch_size 4 \
+    --learning_rate 2e-5 \
+    --weight_decay 0.01 \
+    --num_gpu_devices ${num_gpu} \
+    --num_trigger_tokens 10 \
+    --num_candidates ${candidate_num} \

fe093e638563d78902c46d22f791b66f5e9862d1
kyra-jade2 <yz709@cam.ac.uk>
2023-01-10 12:30:38 +0000

    update scripts

--- experiments/general/mnli-matched-manual-prompt.sh
+++ experiments/general/mnli-matched-manual-prompt.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --nodes=1
 #SBATCH --time=24:00:00
-#SBATCH --job-name=mm1b64
+#SBATCH --job-name=pm1b64
 #SBATCH --gres=gpu:4
 
 # run the application
@@ -17,18 +17,22 @@ num_gpu=4
 cd /jmain02/home/J2AD015/axf03/yxz79-axf03/nlp-prompt-attack/src
 python3 run.py \
     --random_seed ${seed_all} \
-    --task_name "mnli-mismatched-roberta-large-no-prompt-k"${k_all}"-seed"${seed_all} \
+    --task_name "mnli-matched-roberta-large-manual-prompt-k"${k_all}"-seed"${seed_all} \
     --model_name_or_path "roberta-large" \
-    --dataset_name "MNLI-MISMATCHED" \
-    --data_path "/jmain02/home/J2AD015/axf03/yxz79-axf03/nlp-prompt-attack/datasets/k_shot/k="${k_all}"/seed="${seed_all}"/MNLI-MISMATCHED" \
+    --dataset_name "MNLI-MATCHED" \
+    --data_path "/jmain02/home/J2AD015/axf03/yxz79-axf03/nlp-prompt-attack/datasets/k_shot/k="${k_all}"/seed="${seed_all}"/MNLI-MATCHED" \
     --n_classes 3 \
     --do_k_shot \
     --k_samples_per_class ${k_all} \
     --do_train \
     --do_test \
+    --with_prompt \
+    --prompt_type "manual_prompt" \
+    --template "<cls> <premise> ? <mask> , <hypothesis> ." \
+    --verbalizer_dict '{"0":["Yes"], "1":["Maybe"], "2":["No"]}' \
     --log_every_n_steps 20 \
     --val_every_n_steps 20 \
-    --warmup_percent 20 \
+    --warmup_percent 10 \
     --weight_decay 0.01 \
     --max_epoch 100 \
     --max_token_count 512 \

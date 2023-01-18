01331b518805d6a025bb51db4c981c6c92b8f0c8
kyra-at-beyla <yz709@cam.ac.uk>
2023-01-18 00:24:33 +0000

    script to visualise manual prompt mask embedding

--- experiments/mask-visual/run-beyla.sh
+++ experiments/mask-visual/run-beyla.sh
@@ -14,11 +14,12 @@ k_all=16
 
 python3 run.py \
     --random_seed ${seed_all} \
-    --task_name "sst2-roberta-base-backdoor-visual-manual-k"${k_all}"-seed"${seed_all} \
+    --task_name "sst2-roberta-base-visual-manual-k"${k_all}"-seed"${seed_all} \
     --model_name_or_path "roberta-base" \
     --dataset_name "SST2" \
     --data_path "/local/scratch-3/yz709/nlp-prompt-attack/datasets/k_shot/k="${k_all}"/seed="${seed_all}"/SST2" \
     --ckpt_path "/local/scratch-3/yz709/nlp-prompt-attack/src/checkpoints/1-14/sst2-roberta-base-backdoor-visual-manual-k16-seed42/sst2-roberta-base-backdoor-visual-manual-k16-seed42-date=1-14-epoch=07-val_loss=0.39.ckpt" \
+    --do_test \
     --n_classes 2 \
     --do_k_shot \
     --k_samples_per_class ${k_all} \
@@ -36,7 +37,5 @@ python3 run.py \
     --learning_rate 2e-5 \
     --weight_decay 0.01 \
     --num_gpu_devices ${num_gpu} \
-    --backdoored \
-    --target_label 1 \
-    --poison_trigger_list '["cf", " "]' \
+    --visualise \
     1> ${dir}/cl_job_output/${month_day}/log_${time}.out 2>&1

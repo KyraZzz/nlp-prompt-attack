af50a2c6ee11d9c0635ae66362a271d07b2ae77d
kyra-jade2 <yz709@cam.ac.uk>
2023-01-29 12:13:24 +0000

    update scripts

--- experiments/backdoor-auto/run-beyla.sh
+++ experiments/backdoor-auto/run-beyla.sh
@@ -14,32 +14,27 @@ k_all=16
 
 python3 run.py \
     --random_seed ${seed_all} \
-    --task_name "enron-spam-roberta-base-backdoor-manual-k"${k_all}"-seed"${seed_all} \
+    --task_name "enron-spam-roberta-base-auto-k"${k_all}"-seed"${seed_all} \
     --model_name_or_path "roberta-base" \
     --dataset_name "ENRON-SPAM" \
     --data_path "/local/scratch-3/yz709/nlp-prompt-attack/datasets/k_shot/k="${k_all}"/seed="${seed_all}"/ENRON-SPAM" \
-    --ckpt_path "/local/scratch-3/yz709/nlp-prompt-attack/src/backdoored-PLM/roberta-base-maxTokenLen256-seed42" \
     --do_train \
     --do_test \
     --n_classes 2 \
     --do_k_shot \
     --k_samples_per_class ${k_all} \
-    --do_train \
-    --do_test \
     --with_prompt \
-    --prompt_type "manual_prompt" \
-    --template "<cls> <poison> This is a <mask> email: <text> ." \
+    --prompt_type "auto_prompt" \
+    --template "<cls> <mask> <T> <T> <T> <text> ." \
     --verbalizer_dict '{"0":["Ġgenuine"], "1":["Ġspam"]}' \
     --max_token_count ${max_token} \
     --log_every_n_steps 20 \
     --val_every_n_steps 20 \
-    --warmup_percent 10 \
-    --max_epoch 100 \
-    --early_stopping_patience 5 \
     --batch_size 4 \
     --learning_rate 2e-5 \
-    --weight_decay 0.01 \
-    --num_gpu_devices ${num_gpu} \
-    --backdoored \
-    --target_label 0 \
+    --num_gpu_devices 1 \
+    --max_epoch 5 \
+    --early_stopping_patience 5 \
+    --num_trigger_tokens 3 \
+    --num_candidates 10 \
     1> ${dir}/cl_job_output/${month_day}/log_${time}.out 2>&1

6ad172cb3724fabcc5519e255d0ca8343354a18b
KyraZzz <yz709@cam.ac.uk>
2023-04-11 15:16:28 +0800

    updates

--- tests/test_dataset.py
+++ tests/test_dataset.py
@@ -1,3 +1,24 @@
 import pytest
-import sys
-sys.path.insert(0, "src")
+from src.dataset import TextEntailDataset
+import torch
+import pytest
+from transformers import AutoTokenizer
+import random
+random.seed(42)
+
+
+@pytest.mark.parametrize(
+    "data", ["/Users/Kyra_ZHOU/Desktop/Dissertation/code_repo/nlp-prompt-attack/datasets/k_shot/QNLI/train"]
+)
+@pytest.mark.parametrize(
+    "max_token_count", [10]
+)
+@pytest.mark.parametrize(
+    "index", [random.randint(0, 16) for _ in range(5)]
+)
+def test___getitem__(data, max_token_count, index):
+    tokenizer = AutoTokenizer.from_pretrained("roberta-large")
+    dataset = TextEntailDataset(data, tokenizer, max_token_count)
+    value_dict = dataset.__getitem__(index)
+    print(value_dict.items())
+    assert False
